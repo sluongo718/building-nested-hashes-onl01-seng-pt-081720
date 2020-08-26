734891a4e56846767784e3e132cecf74655efc69
sam l <sluongo718@yahoo.com>
2020-08-26 21:15:14 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,15 +1,15 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriarch => {},
-       :matriarch => {},
-       :hero => {},
+       :patriarch => {name: "Lord Montague", age: "53"},
+       :matriarch => {name: "Lady Montague", age: "54"},
+       :hero => {name: "Romeo", age: "15", statue: "alive"},
        :hero_friend => []
      },
      :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :herione => {},
+       :patriarch => {name: "Lord Capulet", age: "50"},
+       :matriarch => {name: "Lady Capulet", age: "51"},
+       :herione => {name: "Juliet", age: "15", status: "alive"},
        :herione_friend => []
      }
    }
